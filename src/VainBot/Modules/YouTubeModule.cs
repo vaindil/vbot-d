4f3f2d2d882b593ecdadf869d5769efdef94025f
Steven Hall <vaindil@gmail.com>
2019-03-02 11:38:01 -0500

    remove unnecessary usings

--- src/VainBot/Modules/YouTubeModule.cs
+++ src/VainBot/Modules/YouTubeModule.cs
@@ -1,9 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using VainBot.Classes.YouTube;
 using VainBot.Services;
