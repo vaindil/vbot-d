8bf08036d16d67c65afcc917fe2681a94a66a315
Steven Hall <vaindil@pm.me>
2020-03-30 19:22:03 -0400

    add !speechless

---
+++ src/VainBot/Modules/SpeechlessModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace VainBot.Modules
+{
+    public class SpeechlessModule : ModuleBase
+    {
+        [Command("speechless")]
+        [Alias("sl", "what", "what?")]
+        public async Task Speechless([Remainder]string unused = null)
+        {
+            await ReplyAsync("https://vaindil.com/sl.gif");
+        }
+    }
+}
