ddc6fccfd2b218445561178f4004f2a51b0267bb
Steven Hall <vaindil@pm.me>
2021-04-18 20:20:01 -0400

    add !carlitos

---
+++ src/VainBot/Modules/CarlitosModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using VainBot.Preconditions;
+
+namespace VainBot.Modules
+{
+    [ZubatGuild]
+    public class CarlitosModule : ModuleBase
+    {
+        [Command("carlitos")]
+        [Alias("carlos", "david", "globes")]
+        public async Task Carlitos([Remainder]string _ = null)
+        {
+            await ReplyAsync("<:Bedge:833494776137121834><:thought1:833494788174249985> Carlitos getting owned by globes <:thought2:833494796156272652>");
+        }
+    }
+}
---
+++ src/VainBot/Preconditions/ZubatGuildAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace VainBot.Preconditions
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class ZubatGuildAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(
+            ICommandContext context,
+            CommandInfo command,
+            IServiceProvider services)
+        {
+            if (context.Guild.Id == 375753810246631445)
+                return Task.FromResult(PreconditionResult.FromSuccess());
+
+            return Task.FromResult(PreconditionResult.FromError("Command cannot be used in this server."));
+        }
+    }
+}
